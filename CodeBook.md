27089b3cb76dcca9a922ec54da53af4707eae647
connielye <connielye@hotmail.com>
2015-10-24 13:01:45 +0200

    first commit

---
+++ CodeBook.md
@@ -0,0 +1,19 @@
+Getting and Cleaning Data Project
+
+Source Data: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+Data Description: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+After setting the source directory for the files, read into tables the data located in
+dataAT
+dataST
+dataFT
+
+Section 1. Extract only the measurements on the mean and standard deviation for each measurement.
+
+Section 2. Use descriptive activity names to name the activities in the data set
+
+Section 3. Appropriately label the data set with descriptive activity names.
+
+Section 4. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+Per the project instructions, we need to produce only a data set with the average of each veriable for each activity and subject
---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+
+
+dataAT <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+
+dataST <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+
+dataFT <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+names(dataST)<-c("subject")
+names(dataAT)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataST, dataAT)
+Data <- cbind(dataFeatures, dataCombine)
+
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
